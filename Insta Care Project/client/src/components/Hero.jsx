9944d58671ab35a1d8994e5f2396b2c370598cee
Phoenixbolt <akshatchandel2003@gmail.com>
2024-05-06 19:09:14 +0530

    Uploaded project from local to git hub

---
+++ Insta Care Project/client/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import image from "../images/heroimg.jpg";
+import "../styles/hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>
+          Your Health, <br />
+          Our Responsibility
+        </h1>
+        
+      </div>
+      <div className="hero-img">
+        <img
+          src={image}
+          alt="hero"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
